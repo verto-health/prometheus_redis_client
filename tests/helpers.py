d4d46e32fd3262cca8cb248aed7daa3eae98e7ff
Sauban Sakhi <85883471+ssakhi@users.noreply.github.com>
2024-02-13 16:26:24 -0500

    fix: update dependencies and add run tox github action

--- tests/helpers.py
+++ tests/helpers.py
@@ -6,7 +6,7 @@ import prometheus_redis_client as prom
 
 @contextmanager
 def MetricEnvironment():
-    redis_client = redis.from_url("redis://redis:6379")
+    redis_client = redis.from_url("redis://localhost:6379")
     redis_client.flushdb()
     refresher = prom.Refresher(refresh_period=2)
     prom.REGISTRY.set_redis(redis_client)
